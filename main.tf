2786e1f3238a61d44dfa1229899d30a6fe2e1c18
Bruno Rossi <brunorossiweb@gmail.com>
2020-01-28 22:26:24 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region  = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
 module "acm_request_certificate" {
   source                            = "app.terraform.io/nicethedevops/acm-request-certificate/aws"
   version                           = "0.4.0"
